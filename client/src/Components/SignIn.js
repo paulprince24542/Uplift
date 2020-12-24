9de01a58f7454f46b8b35d03d2f41ca4c1c865e8
Dibinjose07 <dibinantonyjose@gmail.com>
2020-12-24 20:46:21 +0530

    Signin page edition

--- client/src/Components/SignIn.js
+++ client/src/Components/SignIn.js
@@ -1,36 +1,35 @@
 import React from "react";
-// import SignUp from './SignUp';
 import { Link } from "react-router-dom";
 
 export default function SignIn() {
   return (
-   
-      <div className="login-box">
-        <form>
-          <div className="form-floating mb-3">
-            <input
-              type="email"
-              className="form-control"
-              id="floatingInput"
-              placeholder="name@example.com"
-            />
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div className="form-floating">
-            <input
-              type="password"
-              className="form-control"
-              id="floatingPassword"
-              placeholder="Password"
-            />
-            <label for="floatingPassword">Password</label>
-          </div>
-          <button>SignIn</button>
-        </form>
-        <Link to="/SignUp"><h5>New to <b>Uplift</b> ?</h5></Link>
-
-      </div>
-      
-   
+    <div className="login-box">
+      <form>
+        <div className="form-floating mb-3">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+        <button>SignIn</button>
+      </form>
+      <Link to="/SignUp">
+        <h5 className="mt-3 text-center">
+          New to <b>Uplift</b> ?
+        </h5>
+      </Link>
+    </div>
   );
 }
